546d443b52ca921e3124c93c7a0a5c8a1b1289ea
fulail-kt <muhamedfulail77@gmail.com>
2024-12-17 10:29:11 +0530

    checkout page error

--- src/app/(user)/item/[Id]/page.tsx
+++ src/app/(user)/item/[Id]/page.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import ProductDetails from '../../../_components/productDetails'
+import React from 'react';
+import ProductDetails from '../../../_components/productDetails';
 import { getSession } from '~/server/auth/config';
 
+interface PageProps {
+  params: {
+    Id: string;
+  };
+}
 
-const page = async ({params}:{params:{Id:string}}) => {
-
+const Page = async ({ params }: PageProps) => {
   const session = await getSession();
-console.log(session);  // This will log the company's _id
-// console.log(session.);   // This will log the user's role
+  console.log(session); // This will log the company's _id
+  // console.log(session.role); // Log user's role if needed
   
   return (
     <div>
-        <ProductDetails productId={params.Id}/>
+      <ProductDetails productId={params.Id} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
