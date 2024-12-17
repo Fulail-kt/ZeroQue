6a7c8bb7ef3b2fba630019b4e603909b197b01ac
fulail-kt <muhamedfulail77@gmail.com>
2024-12-17 17:06:55 +0530

    udpated

--- src/app/(user)/item/[Id]/page.tsx
+++ src/app/(user)/item/[Id]/page.tsx
@@ -1,5 +1,7 @@
 // 'use client'
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import ProductDetails from "~/app/_components/productDetails";
 
 // import { useRouter } from 'next/router';
