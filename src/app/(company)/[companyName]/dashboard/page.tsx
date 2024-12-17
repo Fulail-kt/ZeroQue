33fb1de29a1f9491e1c10741e94763cfb9ec1e55
fulail-kt <muhamedfulail77@gmail.com>
2024-12-17 15:02:18 +0530

    updated dashboard chart

--- src/app/(company)/[companyName]/dashboard/page.tsx
+++ src/app/(company)/[companyName]/dashboard/page.tsx
@@ -20,15 +20,6 @@ import {
   TabsList, 
   TabsTrigger 
 } from "~/components/ui/tabs";
-// import {
-//     ChartConfig,
-//     ChartContainer,
-//     ChartLegend,
-//     ChartLegendContent,
-//     ChartTooltip,
-//     ChartTooltipContent,
-//   } from "~/components/ui/chart"
-import { Bar, BarChart, XAxis, YAxis } from "recharts";
 import { 
   LayoutGrid, 
   ShoppingCart, 
