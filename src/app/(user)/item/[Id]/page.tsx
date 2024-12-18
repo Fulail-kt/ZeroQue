c58156f0ebb694a4f89814a8fe0c5da5114142fe
fulail-kt <muhamedfulail77@gmail.com>
2024-12-18 10:56:07 +0530

    udpated

--- src/app/(user)/item/[Id]/page.tsx
+++ src/app/(user)/item/[Id]/page.tsx
@@ -1,28 +1,9 @@
-// 'use client'
+import React from 'react'
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
+export default async function Page({params}:{params:Promise<{Id:string}>}){
+    const name = (await params).Id
+  return (
+    <div>{name}</div>
+  )
+}
 
-import ProductDetails from "~/app/_components/productDetails";
-
-// import { useRouter } from 'next/router';
-
-// import React from 'react'
-// import ProductDetails from '~/app/_components/productDetails';
-
-// const Page = () => {
-//     const router = useRouter();
-//     const productId = router.query.Id as string;
-//   return (
-//     <div><ProductDetails productId={productId} /></div>
-//   )
-// }
-
-// export default Page
-
-// src/app/product/[id]/page.tsx
-//@ts-nocheck
-export default function Page({ params }: { params: { id: string } }) {
-    const productId = params.id;
-    
-    return <ProductDetails productId={productId} />;
-  }
